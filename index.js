7956f3a4a536d826abb2c061bb4a396aed8ed520
= <lukehoogenboom@gmail.com>
2024-03-05 11:00:54 +0100

    added nearest colors aswel as rgb and hsl codes

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const ntc = require('ntcjs');
 const customColors = require('./customColors.js');
+const nearestColors = require('./nearestCOlorFInder.js'); // damm im good at typing
+const utils = require('./colorUTils.js');
+const hexToHsl = require('hex-to-hsl');
+
 const express = require("express");
 
 const app = express();
@@ -13,6 +17,9 @@ app.listen(PORT, () => {
 
   app.get("/picker", (request, response) => {
    let source = request.query.sourceColor;
+   let nearestColorAmount = request.query.nearestColorsAmount ?? 1;
+   let allowedColors = request.query.allowedColors ?? ['#008dcd','#0249BA','#027DBA'];
+
      const status = {
         "Status": "Running"
      };
@@ -23,9 +30,14 @@ app.listen(PORT, () => {
      if(customNamesKeys.includes(source)) {
       name = customColors.names[source];
      }
-     
+
+     let nearest = nearestColors.closestColors(source, allowedColors, nearestColorAmount);
+
      response.send({
       sourceColor: source,
+      rgb: utils.hexToRgb(source),
+      hsl: hexToHsl(source),
       colorName: name,
+      nearestColors: nearest,
      });
   });
