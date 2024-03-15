f273079e1eff2fb1a195aa62c2ebdf5aa739e342
= <lukehoogenboom@gmail.com>
2024-03-15 14:02:25 +0100

    made ai a little bit beter

--- colorClassifier.js
+++ colorClassifier.js
@@ -5,20 +5,23 @@ const utils = require('./colorUTils.js');
 var net = new brain.NeuralNetwork();
 
 net.train([
-    {input: {'r': 255, 'g': 0, 'b': 0}, output: {'red':1}},
-    {input: {'r': 0, 'g': 255, 'b': 0}, output: {'green':1}},
-    {input: {'r': 0, 'g': 0, 'b': 255}, output: {'blue':1}},
-    {input: {'r': 0, 'g': 0, 'b': 0}, output: {'black':1}},
-    {input: {'r': 255, 'g': 255, 'b': 255}, output: {'white':1}},
-    {input: {'r': 255, 'g': 255, 'b': 255}, output: {'white':1}},
-    {input: {'r': 45, 'g': 204, 'b': 191}, output: {'aqua':1}},
-    {input: {'r': 183, 'g': 0, 'b': 244}, output: {'purple':1}}
+    {input: {'r': 0.255, 'g': 0.0, 'b': 0.0}, output: {'Red':1}},
+    {input: {'r': 0.0, 'g': 0.255, 'b': 0.0}, output: {'Green':1}},
+    {input: {'r': 0.0, 'g': 0.0, 'b': 0.255}, output: {'Blue':1}},
+    {input: {'r': 0.0, 'g': 0.0, 'b': 0.0}, output: {'Black':1}},
+    {input: {'r': 0.255, 'g': 0.255, 'b': 0.255}, output: {'White':1}},
+    {input: {'r': 0.255, 'g': 0.255, 'b': 0.255}, output: {'White':1}},
+    {input: {'r': 0.45, 'g': 0.204, 'b': 0.191}, output: {'Aqua':1}},
+    {input: {'r': 0.183, 'g': 0.0, 'b': 0.244}, output: {'Purple':1}}
 ]);
 
 function classifyColor (color) {
     let rgb = utils.hexToRgb(color);
     let output = net.run(rgb);
     let result = Math.max(... Object.values(output));
+
+    console.log(rgb);
+    console.log(output);
 
     let i;
     for(i of Object.keys(output)) {
