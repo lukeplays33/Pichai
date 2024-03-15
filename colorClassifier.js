a287892b87962f93df8ef81ca35dd21bc5bd82af
= <lukehoogenboom@gmail.com>
2024-03-15 13:56:31 +0100

    added colorClass, but AI needs more training.

--- colorClassifier.js
+++ colorClassifier.js
@@ -4,14 +4,29 @@ const utils = require('./colorUTils.js');
 
 var net = new brain.NeuralNetwork();
 
-net.train([{
-    input: {}, output: {'red':1}
-}
+net.train([
+    {input: {'r': 255, 'g': 0, 'b': 0}, output: {'red':1}},
+    {input: {'r': 0, 'g': 255, 'b': 0}, output: {'green':1}},
+    {input: {'r': 0, 'g': 0, 'b': 255}, output: {'blue':1}},
+    {input: {'r': 0, 'g': 0, 'b': 0}, output: {'black':1}},
+    {input: {'r': 255, 'g': 255, 'b': 255}, output: {'white':1}},
+    {input: {'r': 255, 'g': 255, 'b': 255}, output: {'white':1}},
+    {input: {'r': 45, 'g': 204, 'b': 191}, output: {'aqua':1}},
+    {input: {'r': 183, 'g': 0, 'b': 244}, output: {'purple':1}}
 ]);
 
 function classifyColor (color) {
-    var output = net.run(utils/utils.hexToRgb(color)); // { white: 0.99, black: 0.002 }
-      console.log(output)
+    let rgb = utils.hexToRgb(color);
+    let output = net.run(rgb);
+    let result = Math.max(... Object.values(output));
+
+    let i;
+    for(i of Object.keys(output)) {
+        if(output[i] == result) {
+            return i;
+        }
+    }
+
 }
 
 module.exports = { classifyColor };
