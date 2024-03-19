9bbd4d8c5f57457770b0dca0aee0eeff452e9aeb
= <lukehoogenboom@gmail.com>
2024-03-19 12:11:04 +0100

    updated to new names

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-import { PichaiUX } from 'https://lukeplays33.github.io/Pichai-UX/init.js';
+import { PichaiUX } from 'https://lukeplays33.github.io/Pichai-UX/imports.js';
 
 let pichai = new PichaiUX().initialize();
