c9f4b275c6e39934ba6491c2fd9a9f8b0c60c81e
= <lukehoogenboom@gmail.com>
2024-03-20 14:09:55 +0100

    test2

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 import { PichaiUX } from 'https://lukeplays33.github.io/Pichai-UX/imports.js';
 
-alert(document.body.style.backgroundImage)
-let image = String(document.body.style.backgroundImage);
-image = image.substring(3, image.length - 1);
-
-let options = {
-    source: image || '#008dcd',
-    darkMode: window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches,
-    overrideColorsOnScroll: true
-}
-
 window.onload = function () {
-let pichai = new PichaiUX();
-pichai.initialize();
+    alert(document.body.style.backgroundImage)
+    let image = String(document.body.style.backgroundImage);
+    image = image.substring(3, image.length - 1);
+
+    let options = {
+        source: image || '#008dcd',
+        darkMode: window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches,
+        overrideColorsOnScroll: true
+    }
+
+    let pichai = new PichaiUX();
+    pichai.initialize();
 }
