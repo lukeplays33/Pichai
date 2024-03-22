beb9ea8a4e315b5bd0713271e3617faab3ddd95c
= <lukehoogenboom@gmail.com>
2024-03-22 09:34:34 +0100

    made favICon themer a functio now

--- index.js
+++ index.js
@@ -3,18 +3,19 @@ import { PichaiUX } from 'https://lukeplays33.github.io/Pichai-UX/imports.js';
 let pichai = new PichaiUX();
 pichai.initialize();
 
-//update favIcon to match themed one.
-const faviconLink = document.querySelector("link[rel='icon']") || document.querySelector("link[rel='shortcut icon']");
+async function makeThemedfavIcon() {
+    //update favIcon to match themed one.
+    const faviconLink = document.querySelector("link[rel='icon']") || document.querySelector("link[rel='shortcut icon']");
 
-// Get the favicon URL
-const faviconUrl = faviconLink ? faviconLink.href : null;
+    // Get the favicon URL
+    const faviconUrl = faviconLink ? faviconLink.href : null;
 
-pichai.generateDynamicIcon(faviconUrl).then(function (icon) {
     var link = document.querySelector("link[rel~='icon']");
-if (!link) {
-    link = document.createElement('link');
-    link.rel = 'icon';
-    document.head.appendChild(link);
+    if (!link) {
+        link = document.createElement('link');
+        link.rel = 'icon';
+        document.head.appendChild(link);
+    }
+    link.href = await pichai.generateDynamicIcon(faviconUrl);
 }
-link.href = icon;
-});
+makeThemedfavIcon();
