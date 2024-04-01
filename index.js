eb81a3201b749d86b068253f80d9a120c3f0436e
= <lukehoogenboom@gmail.com>
2024-04-01 19:33:33 +0200

    - Added a new paragraph element with the id "colorClass" - Displays the text "Magenta" on the page - Imported a new module for color class finder functionality - Updated assignment of RGB values in the code

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { PichaiUX } from 'https://lukeplays33.github.io/Pichai-UX/imports.js';
+import { findColorClass } from 'https://lukeplays33.github.io/Pichai-UX/AI/colorClassFinder.js';
 import { ntc } from './js/ntc.js';
 import { hexToRgb } from './js/colorUtils.js';
 
@@ -10,12 +11,15 @@ let picker = document.getElementById('colorPicker');
 let name = document.getElementById('colorName');
 let hex = document.getElementById('hex');
 let rgb = document.getElementById('rgb');
+let colorClass = dpcument.getElementById('colorClass');
 
     let n_match = ntc.name(window.localStorage.getItem('previousColor') ?? '#008dcd');
 
+    let RGB = hexToRgb(n_match[0]);
     name.innerHTML = n_match[1];
-    rgb.value = hexToRgb(n_match[0]);
+    rgb.value = RGB
     hex.value = window.localStorage.getItem('previousColor') ?? '#008dcd';
+    colorClass.innerHTML = findColorClass(RGB.substring(4, RGB.length - 1).split(','))
 
 picker.onclick = function () {
     if (!window.EyeDropper) {
@@ -32,10 +36,12 @@ picker.onclick = function () {
             window.localStorage.setItem('lastColor', result.sRGBHex);
             //result.sRGBHex to get hex code
             let n_match = ntc.name(result.sRGBHex);
+            let RGB = hexToRgb(n_match[0]);
 
             name.innerHTML = n_match[1];
-            rgb.value = hexToRgb(n_match[0]);
+            rgb.value = RGB
             hex.value = result.sRGBHex;
+            colorClass.innerHTML = findColorClass(RGB.substring(4, RGB.length - 1).split(','))
         })
         .catch((e) => {
             console.log(e)
