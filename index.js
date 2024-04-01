ad1ebc4c9be4c4edbe8db20f4af707058bed6271
= <lukehoogenboom@gmail.com>
2024-04-01 16:18:02 +0200

    color picker test

--- index.js
+++ index.js
@@ -2,3 +2,22 @@ import { PichaiUX } from 'https://lukeplays33.github.io/Pichai-UX/imports.js';
 
 let pichai = new PichaiUX();
 pichai.initialize();
+
+let picker = document.getElementById('colorPicker').onclick = function () {
+    if (!window.EyeDropper) {
+        resultElement.textContent =
+          "Your browser does not support the EyeDropper API";
+        return;
+      }
+    
+      const eyeDropper = new EyeDropper();
+    
+      eyeDropper
+        .open()
+        .then((result) => {
+          console.log(result.sRGBHex);
+        })
+        .catch((e) => {
+            console.log(e)
+        });
+}
