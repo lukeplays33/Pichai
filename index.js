7876a99d51d695c8bbb226e811243657c888a5c9
= <lukehoogenboom@gmail.com>
2024-04-01 16:27:05 +0200

    hover testing

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ import { PichaiUX } from 'https://lukeplays33.github.io/Pichai-UX/imports.js';
 let pichai = new PichaiUX();
 pichai.initialize();
 
-let picker = document.getElementById('colorPicker').onclick = function () {
+let picker = document.getElementById('colorPicker');
+
+picker.onmouseover = function () {
     if (!window.EyeDropper) {
         resultElement.textContent =
           "Your browser does not support the EyeDropper API";
@@ -11,13 +13,19 @@ let picker = document.getElementById('colorPicker').onclick = function () {
       }
     
       const eyeDropper = new EyeDropper();
+      const abortController = new AbortController();
     
       eyeDropper
-        .open()
+        .open({ signal: abortController.signal })
         .then((result) => {
-          console.log(result);
+            //result.sRGBHex to get hex code
+            console.log(result.sRGBHex);
         })
         .catch((e) => {
             console.log(e)
         });
+}
+
+picker.onmouseleave = function () {
+    abortController.abort();
 }
