ae4e928d160c46189df407fcd3dba564350b9699
= <lukehoogenboom@gmail.com>
2024-04-01 17:36:12 +0200

    - Added new styling properties for #hex, rgb, and #hrex elements - Included margins, colors, padding, and width calculations - Added color information to the article section with hex and rgb values for #000000 - Refactored EyeDropper functionality to display closest matching color's RGB value and name on web page - Improved user experience

--- index.js
+++ index.js
@@ -7,22 +7,29 @@ pichai.initialize();
 let picker = document.getElementById('colorPicker');
 
 let name = document.getElementById('colorName');
+let hex = document.getElementById('hex');
+let rgb = document.getElementById('rgb');
 
 picker.onmouseover = function () {
     if (!window.EyeDropper) {
         resultElement.textContent =
-          "Your browser does not support the EyeDropper API";
+            "Your browser does not support the EyeDropper API";
         return;
-      }
-    
-      const eyeDropper = new EyeDropper();
-    
-      eyeDropper
+    }
+
+    const eyeDropper = new EyeDropper();
+
+    eyeDropper
         .open()
         .then((result) => {
             //result.sRGBHex to get hex code
-            let n_match = ntc.name("#6195ED");
-            name.innerHTML = n_match
+            let n_match = ntc.name(result.sRGBHex);
+            n_rgb = n_match[0]; // This is the RGB value of the closest matching color
+            n_name = n_match[1]; // This is the text string for the name of the match
+
+            name.innerHTML = n_name;
+            rgb.innerHTML = n_rgb;
+            hex.innerHTML = result.sRGBHex;
         })
         .catch((e) => {
             console.log(e)
