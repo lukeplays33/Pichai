4ba38ee2496eaa8aa9a55f3a49b6cb6597fe0019
= <lukehoogenboom@gmail.com>
2024-04-08 11:10:47 +0200

    typo

--- js/settings.js
+++ js/settings.js
@@ -5,8 +5,7 @@ pichai.initialize();
 
 let lengthInput = document.getElementById('length');
 
-alert(window.localStorage.getItem('palelteLength'))
-lengthInput.value = window.localStorage.getItem('palelteLength'); //set value back to the one set by the user
+lengthInput.value = window.localStorage.getItem('palleteLength'); //set value back to the one set by the user
 
 lengthInput.onchange = function () {
     window.localStorage.setItem('palleteLength', this.value); // save the users selected pallete size
