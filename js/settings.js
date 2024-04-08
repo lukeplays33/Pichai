021c25bc56b136c52eedf3bc887c214359bcc8fd
= <lukehoogenboom@gmail.com>
2024-04-08 13:54:55 +0200

    test

--- js/settings.js
+++ js/settings.js
@@ -6,6 +6,7 @@ pichai.initialize();
 let lengthInput = document.getElementById('length');
 let allowedColors = document.getElementById('allowedColors');
 let allowedColorsList = document.getElementById('allowedColorsDialog');
+let add = document.getElementById('add');
 
 lengthInput.value = window.localStorage.getItem('palleteLength') == null ? 10 : window.localStorage.getItem('palleteLength'); //set value back to the one set by the user
 
@@ -21,4 +22,21 @@ allowedColors.onclick = function () {
         this.value = 'V';
         allowedColorsList.style.display = 'none';
     }
+}
+
+add.onclick = function () {
+    let color = prompt('Enter a hex code to add');
+
+    if(color == '' || color == null) {
+        return;
+    }
+
+    addAllowedColor(color);
+}
+
+function addAllowedColor (color) {
+    let holder = add.cloneNode(true);
+    holder.children[1].value = '-';
+    holder.children[1].id = `remove${color}`;
+    holder.children[0].innerHTML = color;
 }
