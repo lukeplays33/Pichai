a17675dd3bade3078dc65d5fd20a12e0e028816c
= <lukehoogenboom@gmail.com>
2024-04-08 11:07:29 +0200

    bug fix

--- js/settings.js
+++ js/settings.js
@@ -1,3 +1,8 @@
+import { PichaiUX } from 'https://lukeplays33.github.io/Pichai-UX/imports.js';
+
+let pichai = new PichaiUX();
+pichai.initialize();
+
 let lengthInput = document.getElementById('length');
 
 lengthInput.value = window.localStorage.getItem('palelteLength'); //set value back to the one set by the user
