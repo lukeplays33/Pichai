5575bbd3c1fd7650c329a0fd9784107b95597c84
= <lukehoogenboom@gmail.com>
2024-04-08 11:17:12 +0200

    test

--- js/settings.js
+++ js/settings.js
@@ -4,8 +4,8 @@ let pichai = new PichaiUX();
 pichai.initialize();
 
 let lengthInput = document.getElementById('length');
-alert(window.localStorage.getItem('palleteLength'))
-lengthInput.value = window.localStorage.getItem('palleteLength') == '' ? 10 : window.localStorage.getItem('palleteLength'); //set value back to the one set by the user
+
+lengthInput.value = window.localStorage.getItem('palleteLength') == null ? 10 : window.localStorage.getItem('palleteLength'); //set value back to the one set by the user
 
 lengthInput.onchange = function () {
     window.localStorage.setItem('palleteLength', this.value); // save the users selected pallete size
