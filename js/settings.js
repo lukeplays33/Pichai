c2e48d277def51405dc196256aef25450951bccd
= <lukehoogenboom@gmail.com>
2024-04-16 09:58:26 +0200

    test

--- js/settings.js
+++ js/settings.js
@@ -14,7 +14,7 @@ let creteColor = document.getElementById('create');
 let listOfAllowedColors = ['#FD0100', '#0100FC', '#009901', '#F89909', '#FEFD00', '#FFCAD2', '#980086', '#FD83F2', '#00DECF', '#FCD800', '#07FB00', '#05FBFF', '#010084', '#FF7D4C', '#008083', '#C02428', '#FDFDFD', '#020000', '#A39D97'];
 
 if (window.localStorage.getItem('allowedColors') == null || window.localStorage.getItem('allowedColors') == '') {} else {
-    listOfAllowedColors = listOfAllowedColors.concat(window.localStorage.getItem('allowedColors').split(','));
+    listOfAllowedColors = window.localStorage.getItem('allowedColors').split(',');
 }
 
 for (i of listOfAllowedColors) {
