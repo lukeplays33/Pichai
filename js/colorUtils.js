cd0645a22cbefe5a095d17c5f216ecda2f2b7811
= <lukehoogenboom@gmail.com>
2024-05-03 14:46:50 +0200

    added coral to list

--- js/colorUtils.js
+++ js/colorUtils.js
@@ -112,6 +112,7 @@ function cmykToHex(value) {
 let colorAssociations = {
   red: ['Action', 'Strength', 'Energy', 'Passion', 'Attention', 'Motivates', 'Stimulates', 'Cautions', 'Sexuality', 'Courage', 'Desire', 'Confidence', 'Anger', 'Danger', 'Revenge', 'Aggresion'],
   orange: ['Emotion', 'Youth', 'Optimism', 'Enthusiasm', 'Encourages', 'Uplifts', 'Stimulates', 'Communicate', 'Spontaneity', 'Creativity', 'Warmth', 'Positivity', 'Exhibitionsism', 'Superficial', 'Impatient', 'Domination'],
+  coral: ['Emotion', 'Youth', 'Optimism', 'Enthusiasm', 'Encourages', 'Uplifts', 'Stimulates', 'Communicate', 'Spontaneity', 'Creativity', 'Warmth', 'Positivity', 'Exhibitionsism', 'Superficial', 'Impatient', 'Domination'],
 };
 
 export { hexToRgb, RGBToHSL, rgbToCmyk, rgbToHex, hslToHex, cmykToHex, colorAssociations }
