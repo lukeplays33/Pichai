d0633238aa5ea77e9620cd39d05ca16711c6bb62
= <lukehoogenboom@gmail.com>
2024-05-03 14:35:56 +0200

    test

--- js/colorUtils.js
+++ js/colorUtils.js
@@ -111,7 +111,7 @@ function cmykToHex(value) {
 
 let colorAssociations = {
   red: ['Action', 'Strength', 'Energy', 'Passion', 'Attention', 'Motivates', 'Stimulates', 'Cautions', 'Sexuality', 'Courage', 'Desire', 'Confidence', 'Anger', 'Danger', 'Revenge', 'Aggresion'],
-
+  orange: ['Emotion', 'Youth', 'Optimism', 'Enthusiasm', 'Encourages', 'Uplifts', 'Stimulates', 'Communicate', 'Spontaneity', 'Creativity', 'Warmth', 'Positivity', 'Exhibitionsism', 'Superficial', 'Impatient', 'Domination'],
 };
 
 export { hexToRgb, RGBToHSL, rgbToCmyk, rgbToHex, hslToHex, cmykToHex, colorAssociations }
