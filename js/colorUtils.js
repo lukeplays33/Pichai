30b65af3505bd9c7f84031692f7c94178f8834d5
= <lukehoogenboom@gmail.com>
2024-05-03 12:04:40 +0200

    test

--- js/colorUtils.js
+++ js/colorUtils.js
@@ -53,47 +53,24 @@ function RGBToHSL(r, g, b) {
   return `${h}, ${s}, ${l}`;
 }
 
-function rgbToCmyk(r,g,b) {
-  let computedC = 0;
-  let computedM = 0;
-  let computedY = 0;
-  let computedK = 0;
- 
-  //remove spaces from input RGB values, convert to int
- r = parseInt( (''+r).replace(/\s/g,''),10 ); 
- g = parseInt( (''+g).replace(/\s/g,''),10 ); 
- b = parseInt( (''+b).replace(/\s/g,''),10 ); 
- 
-  if ( r==null || g==null || b==null ||
-      isNaN(r) || isNaN(g)|| isNaN(b) )
-  {
-    console.log ('Please enter numeric RGB values!');
-    return;
-  }
-  if (r<0 || g<0 || b<0 || r>255 || g>255 || b>255) {
-    console.log ('RGB values must be in the range 0 to 255.');
-    return;
-  }
- 
-  // BLACK
-  if (r==0 && g==0 && b==0) {
-   computedK = 1;
-   return [0,0,0,1];
-  }
- 
-  computedC = 1 - (r/255);
-  computedM = 1 - (g/255);
-  computedY = 1 - (b/255);
- 
-  var minCMY = Math.min(computedC,
-               Math.min(computedM,computedY));
-  computedC = Math.round((computedC - minCMY) / (1 - minCMY) * 100) ;
-  computedM = Math.round((computedM - minCMY) / (1 - minCMY) * 100) ;
-  computedY = Math.round((computedY - minCMY) / (1 - minCMY) * 100 );
-  computedK = Math.round(minCMY * 100);
- 
-  return `${computedC}, ${computedM}, ${computedY}, ${computedK}`
- }
+function rgbToCmyk(r, g, b) {
+  const c = 1 - r / 255;
+  const m = 1 - g / 255;
+  const y = 1 - b / 255;
+  const k = Math.min(c, Math.min(m, y));
+
+  c = (c - k) / (1 - k);
+  m = (m - k) / (1 - k);
+  y = (y - k) / (1 - k);
+
+  // Optionally, round the values to percentages
+  c = Math.round(c * 100);
+  m = Math.round(m * 100);
+  y = Math.round(y * 100);
+  k = Math.round(k * 100);
+
+  return { c, m, y, k };
+}
 
 function rgbToHex(value) {
   let r = value.split(',')[0];
