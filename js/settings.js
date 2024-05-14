c41d9fb31eadd3ce75b9fe51aa2b17e2428228f4
= <lukehoogenboom@gmail.com>
2024-05-14 11:50:31 +0200

    test

--- js/settings.js
+++ js/settings.js
@@ -12,13 +12,21 @@ let add = document.getElementById('add');
 let creteColor = document.getElementById('create');
 
 let listOfAllowedColors = [];
-listOfAllowedColors = localforage.getItem('allowedColors').split(',');
+listOfAllowedColors = localforage.getItem('allowedColors').then(function(value) {
+    // This code runs once the value has been loaded
+    // from the offline store.
+    console.log(value);
+}); //set value back to the one set by the usersplit(',');
 
 for (i of listOfAllowedColors) {
     addAllowedColor(i);
 }
 
-lengthInput.value = localforage.getItem('palleteLength'); //set value back to the one set by the user
+lengthInput.value = localforage.getItem('palleteLength').then(function(value) {
+    // This code runs once the value has been loaded
+    // from the offline store.
+    console.log(value);
+}); //set value back to the one set by the user
 
 lengthInput.onchange = function () {
     localforage.setItem('palleteLength', this.value); // save the users selected pallete size
