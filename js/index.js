40cabd436f8bc6067ba5e776ab2dace6b4f8b729
= <lukehoogenboom@gmail.com>
2024-06-04 17:43:04 +0200

    yrdy

--- js/index.js
+++ js/index.js
@@ -10,7 +10,6 @@ import { PichaiUX } from 'https://lukeplays33.github.io/Pichai-UX/imports.js';
 
 let pichai = new PichaiUX();
 pichai.initialize();
-console.log(pichai.getListOfElements());
 
 let i;
 
