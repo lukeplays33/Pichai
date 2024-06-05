29c039567632ed65ab3a2fa0208ecfaae3a02636
= <lukehoogenboom@gmail.com>
2024-06-05 13:42:26 +0200

    test

--- js/index.js
+++ js/index.js
@@ -10,6 +10,7 @@ import { PichaiUX } from 'https://lukeplays33.github.io/Pichai-UX/imports.js';
 
 let pichai = new PichaiUX();
 pichai.initialize();
+console.log(pichai.getListOfElements());
 
 let i;
 
