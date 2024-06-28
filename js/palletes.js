6bcd04e5ec9108e80638f569eea2bb68adfb1041
= <lukehoogenboom@gmail.com>
2024-06-28 09:58:25 +0200

    test

--- js/palletes.js
+++ js/palletes.js
@@ -2,3 +2,12 @@ import { PichaiUX } from 'https://lukeplays33.github.io/Pichai-UX/imports.js';
 
 let pichai = new PichaiUX();
 pichai.initialize();
+
+function getTypeFromUrl(name){
+    if(name=(new RegExp('[?&]'+encodeURIComponent(name)+'=([^&]*)')).exec(location.search))
+       return decodeURIComponent(name[1]);
+ }
+
+ if(getTypeFromUrl() == 'ux') {
+    document.getElementById('palletePager').pageIndex = 1;
+ }
