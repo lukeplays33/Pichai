0fc027155df751afb65af0a995b3231ed89fe2e4
= <lukehoogenboom@gmail.com>
2024-07-19 11:59:34 +0200

    added an option to disable homeLinking in the header

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@ import { getSimilarColors, addTile, colourBlend } from './utils.js';
 
 import { PichaiUX } from 'https://lukeplays33.github.io/Pichai-UX/imports.js';
 
-let options = {homeLink: ''}
+let options = {homeLink: 'https://lukeplays33.github.io/Pichai/index.html'}
 
 let pichai = new PichaiUX(options);
 pichai.initialize();
