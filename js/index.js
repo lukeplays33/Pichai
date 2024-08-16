c60ce703f8abf3ade1dd480eecc30dd1e981878a
= <lukehoogenboom@gmail.com>
2024-08-16 11:30:51 +0200

    test

--- js/index.js
+++ js/index.js
@@ -92,7 +92,7 @@ async function setInnerHTMLs(code) { // updates all elements to match the select
     similarColorHolder.innerHTML = '';
     repeatingColors = [];
     for (i of await getSimilarColors(code)) {
-        console.log(typeof i)
+        console.log(repeatingColors, repeatingColors.includes(i), i)
         if (repeatingColors.includes(i)) { } else {
             addTile(similarColorHolder, i);
             repeatingColors.push(i);
