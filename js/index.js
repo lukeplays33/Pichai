d399f9fa7c8ad13a168dcd01a880055e9fef52a6
luke hoogenboom <6035070@mborijnland.nl>
2024-09-17 15:38:07 +0200

    added contrast checking

--- js/index.js
+++ js/index.js
@@ -255,11 +255,11 @@ function checkAccesibillity(color, textColor) {
                 AAA-level small text: ${ratio < 1 / 7 ? 'PASS' : 'FAIL'}
                `;
 
-    if (ratio < 1 / 3) {
-        scoreCheck.innerHTML = 'cancel';
+    if (ratio < 1 / 3 && ratio < 1 / 4.5 && ratio < 1 / 7) {
+        scoreCheck.innerHTML = 'check_circle';
     } else if (ratio < 1 / 3 && ratio < 1 / 4.5) {
         scoreCheck.innerHTML = 'adjust';
-    } else if (ratio < 1 / 3 && ratio < 1 / 4.5 && ratio < 1 / 7) {
-        scoreCheck.innerHTML = 'check_circle';
+    } else if (ratio < 1 / 3) {
+        scoreCheck.innerHTML = 'cancel';
     }
 }
